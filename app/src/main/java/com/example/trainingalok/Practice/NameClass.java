61bad49d7b5f405a6c7eab6bc6695accb00a6e3e
muzammilahsan96 <muzammilahsan96@gmail.com>
2021-07-15 13:09:03 +0530

    New programs of list added

--- app/src/main/java/com/example/trainingalok/Practice/NameClass.java
+++ app/src/main/java/com/example/trainingalok/Practice/NameClass.java
@@ -9,8 +9,8 @@ public class NameClass {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
-        List<String > names=new ArrayList<>();
-        names.add("raj");
+        List<String > names=new ArrayList<>();  //List Creation
+        names.add("raj");   //Adding Elements
         names.add("ajay");
         names.add("alok");
         names.add("ashish");
@@ -31,17 +31,48 @@ public class NameClass {
         names.add("nikhil");
         names.add("ashutosh");
 
+        NameClass obj=new NameClass();  //Object Creation
+
         System.out.println("Enter string to be searched");
         String s=sc.nextLine();
 
         s=s.trim().toLowerCase();
-        NameClass obj=new NameClass();
+
 
         List<String> ls=obj.findNames(names,s);
         System.out.println(ls);
 
+
+        List<String> names2=new ArrayList<>();
+        names2.add("muzammil");
+        names2.add("muzakkir");
+        names2.add("sachin");
+        names2.add("raj");
+        names2.add("koko");
+
+        List<String> ls1=obj.compareNames(names,names2);
+        System.out.println(ls1);
+
+        System.out.println("Enter page number");
+        int page=sc.nextInt();
+        int a= obj.printpart(page);
+        if(a!=0)
+        {
+            System.out.println("Part Number = "+a);
+        }
+        else
+        {
+            System.out.println("Partition does not exist for given page ");
+        }
+        List<Integer> ar=new ArrayList<>();
+        ar.add(1);
+        ar.add(3);
+        ar.add(15);
+        int sum=obj.addList(ar);
+        System.out.println("Sum="+sum);
+
     }
-    public List<String > findNames(List<String> names,String s)
+    public List<String> findNames(List<String> names,String s)  //Print the names which contains string s
     {
         List<String > ls=new ArrayList<>();
         for(int i=0;i<names.size();i++)
@@ -53,5 +84,49 @@ public class NameClass {
 
         }
         return ls;
+    }
+    public List<String > compareNames(List<String> names1,List<String> names2)  //Print the union of the lists
+    {
+        List<String > ls=new ArrayList<>();
+        int size1=0,size2=0;
+        if(names1.size()>names2.size())
+        {
+            size1=names1.size();
+            size2=names2.size();
+        }
+        else
+        {
+            size1=names2.size();
+            size2=names1.size();
+        }
+        for(int i=0;i<size1;i++)
+        {
+            for(int j=0;j<size2;j++)
+            {
+                if(names1.get(i).equals(names2.get(j)))
+                {
+                    ls.add(names1.get(i));
+                }
+            }
+        }
+        return ls;
+    }
+    public int printpart(int n) //Print the part of the book by knowing page number as parameter. Each part has 10 pages
+    {
+        if(n<1)
+        {
+            return 0;
+        }
+        int a=n/10;
+        return a+1;
+    }
+    public int addList(List<Integer> ls)    //This function return the sum of all the elements present in integer list
+    {
+        int sum=0;
+        for(int i=0;i<ls.size();i++)
+        {
+            sum=sum+ls.get(i);
+        }
+        return sum;
     }
 }
